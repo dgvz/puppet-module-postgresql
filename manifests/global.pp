54433f235635ce79cadca19109e6db173f08b616
Matt Palmer <mpalmer@hezmatt.org>
2014-08-04 15:57:56 +1000

    Allow the specification of, for example, a global locale

---
+++ manifests/global.pp
@@ -0,0 +1,33 @@
+# Configure some "global" PostgreSQL options.
+#
+# Attributes:
+#
+#  * `locale` (string; optional; default `undef`)
+#
+#     Set a specific default locale for all PostgreSQL clusters created on
+#     this machine.
+#
+#  * `encoding` (string; optional; default `undef`)
+#
+#     Set a specific default character encoding for all PostgreSQL clusters
+#     created on this machine.
+#
+class postgresql::global(
+		$locale   = undef,
+		$encoding = undef
+) {
+	package { "postgresql-common": }
+
+	$postgresql_global_locale   = $locale
+	$postgresql_global_encoding = $encoding
+
+	file { "/etc/postgresql-common/createcluster.conf":
+		ensure  => "file",
+		content => template("postgresql/etc/postgresql-common/createcluster.conf"),
+		mode    => 0444,
+		owner   => "root",
+		group   => "root",
+		require => Package["postgresql-common"],
+		before  => Noop["postgresql/server/installed"],
+	}
+}
