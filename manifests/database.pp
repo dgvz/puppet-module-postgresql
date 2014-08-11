5031611c09e1260dc250fc0261be27b7d1ceacbe
Matt Palmer <mpalmer@hezmatt.org>
2014-08-11 14:46:16 +1000

    Sigh

--- manifests/database.pp
+++ manifests/database.pp
@@ -15,7 +15,7 @@ define postgresql::database(
 	if $extensions {
 		exec { "${name}::Set extensions for ${name} DB to ${extensions}":
 			command => "/bin/su -l postgres -c \"/usr/local/sbin/set-pg-db-extensions ${name} ${extensions}\"",
-			unless  => "/bin/sh -l postgres -c \"/usr/local/sbin/check-pg-db-extensions ${name} ${extensions}\"",
+			unless  => "/bin/su -l postgres -c \"/usr/local/sbin/check-pg-db-extensions ${name} ${extensions}\"",
 			require => [Service["postgresql"],
 			            File["/usr/local/sbin/set-pg-db-extensions"],
 			            File["/usr/local/sbin/check-pg-db-extensions"],
